e36a4ed5c9e2c69b2fbe2e3b35e7095042ae46bb
ValDavSto <vdstolz@gmail.com>
2024-06-01 12:24:01 +0200

    Strucutre methodes added

---
+++ Project-Digital-Twin-Model-Convertion/src/code/datastructure/Gds2Structure.cpp
@@ -0,0 +1,1 @@
+#include "Gds2Structure.h"
--- Project-Digital-Twin-Model-Convertion/src/header/Gds2Structure.h
+++ Project-Digital-Twin-Model-Convertion/src/header/Gds2Structure.h
@@ -10,6 +10,15 @@ private:
 	std::string name;
 	std::vector<Polygon> polygons;
 	std::vector<Gds2Structure> structureReferences;
+public:
+	Gds2Structure(std:: string name, std::vector<Polygon> polygons);
+
+	std::string getName();
+	std::vector<Polygon> getPolygons();
+	std::vector<Gds2Structure> getStructRef();
+
+	void setStructRef(std::vector<Gds2Structure> structRef);
 };
+
 
 #endif
